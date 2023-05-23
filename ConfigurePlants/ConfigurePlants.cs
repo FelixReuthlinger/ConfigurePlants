6e999cf3399fbc3f5d8d12e4024b35c7b383baf2
Felix Reuthlinger <10428747+FelixReuthlinger@users.noreply.github.com>
2023-05-23 14:28:18 +0200

    Update ConfigurePlants.cs

--- ConfigurePlants/ConfigurePlants.cs
+++ ConfigurePlants/ConfigurePlants.cs
@@ -5,26 +5,19 @@ using Jotunn.Utils;
 
 namespace ConfigurePlants
 {
-    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
     [BepInDependency(Jotunn.Main.ModGuid)]
-    //[NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
+    [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
     internal class ConfigurePlants : BaseUnityPlugin
     {
-        public const string PluginGUID = "com.jotunn.ConfigurePlants";
-        public const string PluginName = "ConfigurePlants";
-        public const string PluginVersion = "0.0.1";
-        
-        // Use this class to add your own localization to the game
-        // https://valheim-modding.github.io/Jotunn/tutorials/localization.html
-        public static CustomLocalization Localization = LocalizationManager.Instance.GetLocalization();
+        private const string PluginAuthor = "FixItFelix";
+        private const string PluginName = "ConfigurePlants";
+        private const string PluginVersion = "1.0.0";
+        public const string PluginGuid = PluginAuthor + "." + PluginName;
 
         private void Awake()
         {
-            // Jotunn comes with its own Logger class to provide a consistent Log style for all mods using it
             Jotunn.Logger.LogInfo("ConfigurePlants has landed");
-            
-            // To learn more about Jotunn's features, go to
-            // https://valheim-modding.github.io/Jotunn/tutorials/overview.html
         }
     }
 }
